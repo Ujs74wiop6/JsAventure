2ae83433afa8265b17acd6e17888fc699ff20a8c
Fabricio Tiago Arantes <fabricioujs74@gmail.com>
2022-05-07 19:50:40 -0300

    commit

---
+++ helloWorldJs.js
@@ -0,0 +1,24 @@
+//Aqui vou estar testando, e aprendendo novas funções
+//com o decorrer do tempo e que eu for me aprimorando vou atualizando aqui... 
+var message = "Hello world"
+var idade = 18
+var nome = "Fabricio"
+console.log(message)
+
+// concatenação
+console.log("Eu sou " + nome)
+
+//estrutura de condição
+if (idade >= 18) {
+    console.log("Sou maior de idade")
+} else {
+    console.log("Não sou maior de idade")
+}
+
+//laço de repetição
+var tabuada = 7
+var contador = 1
+console.log("Essa é a tabuada do 7")
+for (contador = 1; contador <= 10; contador++) {
+    console.log(tabuada + " X " + contador + " = " + tabuada * contador)
+}
